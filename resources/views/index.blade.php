8cb69643f4c33ffc912763390d71d9a5e6b1e7ab
Yamendra Pun <punyamendra@gmail.com>
2021-04-12 23:03:59 +0900

    Design Single Idea Page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -39,16 +39,18 @@
                     </button>
                 </div>
             </div>
-            <div class="flex px-2 py-6">
-                <a href="#" class="flex-none">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="Avatar" class="w-14 h-14 rounded-xl" />
-                </a>
-                <div class="mx-4">
+            <div class="flex flex-1 px-2 py-6">
+                <div class="flex-none">
+                    <a href="#">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="Avatar" class="w-14 h-14 rounded-xl" />
+                    </a>
+                </div>
+                <div class="w-full mx-4">
                     <h4 class="text-xl font-semibold">
                         <a href="#" class="hover:underline">A random title can go here</a>
                     </h4>
                     <div class="text-gray-600 mt-3 line-clamp-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum et ducimus atque id architecto, inventore deleniti facere, ipsam quam ut minus vero consequuntur non? Reprehenderit, quod possimus? Nostrum itaque, sapiente quibusdam sit ipsa veritatis facilis inventore nesciunt iusto officia nobis cumque iste, atque eaque commodi odit eligendi. Iusto quis ipsa dolores voluptas sed, provident tenetur iste harum repellendus aut magni voluptates quasi reprehenderit maiores alias odio modi perspiciatis a voluptatum cupiditate error rem. Doloribus magnam tempore aperiam vitae minus quasi harum cumque similique perspiciatis laudantium delectus, reprehenderit ducimus temporibus, nostrum cupiditate odit nam distinctio ullam quas velit provident. Expedita, est?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </div>
                     <div class="flex items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
@@ -61,7 +63,7 @@
                         </div>
                         <div class="flex items-center space-x-2">
                             <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">Open</div>
-                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
                                 <svg fill="currentColor" width="24" height="6">
                                     <path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)" />
                                 </svg>
@@ -90,16 +92,18 @@
                     </button>
                 </div>
             </div>
-            <div class="flex px-2 py-6">
-                <a href="#" class="flex-none">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="Avatar" class="w-14 h-14 rounded-xl" />
-                </a>
-                <div class="mx-4">
+            <div class="flex flex-1 px-2 py-6">
+                <div class="flex-none">
+                    <a href="#">
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="Avatar" class="w-14 h-14 rounded-xl" />
+                    </a>
+                </div>
+                <div class="w-full mx-4">
                     <h4 class="text-xl font-semibold">
                         <a href="#" class="hover:underline">Another random title can go here</a>
                     </h4>
                     <div class="text-gray-600 mt-3 line-clamp-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum et ducimus atque id architecto, inventore deleniti facere, ipsam quam ut minus vero consequuntur non? Reprehenderit, quod possimus? Nostrum itaque, sapiente quibusdam sit ipsa veritatis facilis inventore nesciunt iusto officia nobis cumque iste, atque eaque commodi odit eligendi. Iusto quis ipsa dolores voluptas sed, provident tenetur iste harum repellendus aut magni voluptates quasi reprehenderit maiores alias odio modi perspiciatis a voluptatum cupiditate error rem. Doloribus magnam tempore aperiam vitae minus quasi harum cumque similique perspiciatis laudantium delectus, reprehenderit ducimus temporibus, nostrum cupiditate odit nam distinctio ullam quas velit provident. Expedita, est?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
                     </div>
                     <div class="flex items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
@@ -112,7 +116,7 @@
                         </div>
                         <div class="flex items-center space-x-2">
                             <div class="bg-yellow text-xxs font-bold uppercase leading-none rounded-full text-white text-center w-28 h-7 py-2 px-4">In Progress</div>
-                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
                                 <svg fill="currentColor" width="24" height="6">
                                     <path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)" />
                                 </svg>
@@ -138,16 +142,18 @@
                     </button>
                 </div>
             </div>
-            <div class="flex px-2 py-6">
-                <a href="#" class="flex-none">
-                    <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="Avatar" class="w-14 h-14 rounded-xl" />
-                </a>
-                <div class="mx-4">
+            <div class="flex flex-1 px-2 py-6">
+                <div class="flex-none">
+                    <a href="#" >
+                        <img src="https://source.unsplash.com/200x200/?face&crop=face&v=1" alt="Avatar" class="w-14 h-14 rounded-xl" />
+                    </a>
+                </div>
+                <div class="w-full mx-4">
                     <h4 class="text-xl font-semibold">
                         <a href="#" class="hover:underline">Another random title can go here</a>
                     </h4>
                     <div class="text-gray-600 mt-3 line-clamp-3">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum et ducimus atque id architecto, inventore deleniti facere, ipsam quam ut minus vero consequuntur non? Reprehenderit, quod possimus? Nostrum itaque, sapiente quibusdam sit ipsa veritatis facilis inventore nesciunt iusto officia nobis cumque iste, atque eaque commodi odit eligendi. Iusto quis ipsa dolores voluptas sed, provident tenetur iste harum repellendus aut magni voluptates quasi reprehenderit maiores alias odio modi perspiciatis a voluptatum cupiditate error rem. Doloribus magnam tempore aperiam vitae minus quasi harum cumque similique perspiciatis laudantium delectus, reprehenderit ducimus temporibus, nostrum cupiditate odit nam distinctio ullam quas velit provident. Expedita, est?
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. 
                     </div>
                     <div class="flex items-center justify-between mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
@@ -160,7 +166,7 @@
                         </div>
                         <div class="flex items-center space-x-2">
                             <div class="bg-red text-xxs font-bold uppercase leading-none rounded-full text-white text-center w-28 h-7 py-2 px-4">Closed</div>
-                            <button class="relative bg-gray-100 hover:bg-gray-200 rounded-full h-7 transition duration-150 ease-in py-2 px-3">
+                            <button class="relative bg-gray-100 hover:bg-gray-200 border rounded-full h-7 transition duration-150 ease-in py-2 px-3">
                                 <svg fill="currentColor" width="24" height="6">
                                     <path d="M2.97.061A2.969 2.969 0 000 3.031 2.968 2.968 0 002.97 6a2.97 2.97 0 100-5.94zm9.184 0a2.97 2.97 0 100 5.939 2.97 2.97 0 100-5.939zm8.877 0a2.97 2.97 0 10-.003 5.94A2.97 2.97 0 0021.03.06z" style="color: rgba(163, 163, 163, .5)" />
                                 </svg>
