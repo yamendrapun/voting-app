03e6b2eceae8b4bd668c5a4abebf138c03ac6137
Yamendra Pun <punyamendra@gmail.com>
2021-04-13 21:55:28 +0900

    Add Categories

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Idea;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,8 +15,19 @@ class DatabaseSeeder extends Seeder
      * @return void
      */
     public function run()
-    {
-        // \App\Models\User::factory(10)->create();
+    {        
+        User::factory()->create([
+            'name' => 'Yamendra',
+            'email' => 'punyamendra@gmail.com'
+        ]);
+
+        User::factory(19)->create();
+
+        Category::factory()->create(['name' => 'Category 1']);
+        Category::factory()->create(['name' => 'Category 2']);
+        Category::factory()->create(['name' => 'Category 3']);
+        Category::factory()->create(['name' => 'Category 4']);
+        
         Idea::factory(30)->create();
     }
 }
