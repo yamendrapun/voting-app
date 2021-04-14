eac6050e36bc11cbf337614c1511b01eba90503d
Yamendra Pun <punyamendra@gmail.com>
2021-04-14 19:44:05 +0900

    Voting Database Structure

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use App\Models\Idea;
 use App\Models\Status;
 use App\Models\User;
+use App\Models\Vote;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -35,6 +36,18 @@ class DatabaseSeeder extends Seeder
         Status::factory()->create(['name' => 'Implemented', 'classes' => 'bg-green text-white']);
         Status::factory()->create(['name' => 'Closed', 'classes' => 'bg-red text-white']);
         
-        Idea::factory(30)->create();
+        Idea::factory(100)->create();
+
+        // Generate unique votes. Ensure idea_id and user_id are unique for each row
+        foreach (range(1, 20) as $user_id) {
+            foreach (range(1, 100) as $idea_id) {
+                if($idea_id % 2 === 0){
+                    Vote::factory()->create([
+                        'user_id' => $user_id,
+                        'idea_id' => $idea_id,
+                    ]);
+                }
+            }
+        }
     }
 }
